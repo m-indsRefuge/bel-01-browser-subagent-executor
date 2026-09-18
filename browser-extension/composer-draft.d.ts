61828b37fe08ee12d0bffc682a0aac7e3dec2c7a
Nolan Augustine <146803501+m-indsRefuge@users.noreply.github.com>
2026-09-18 06:31:01 +0200

    Type composer draft browser module

---
+++ browser-extension/composer-draft.d.ts
@@ -0,0 +1,7 @@
+export const MAX_DRAFT_CHARACTERS: number
+
+export function validateComposerDraft(text: unknown): string
+
+export function buildComposerDraftWriteExpression(text: string): string
+
+export function buildComposerDraftClearExpression(): string
